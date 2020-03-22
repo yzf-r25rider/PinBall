513fac8fe71f96ef210616a3704f4ad5cc95b4e9
エクバ2勝てないマン <trumanzz7575@gmail.com>
2020-03-22 17:31:47 +0900

    Revert "Revert "Lesson5　課題を提出します。""
    This reverts commit f4611b588b9de31550321702694aa151389793f6.

---
+++ Assets/BallController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    //ポイント変数の宣言
+    int Point = 0;
+
+
+    private GameObject PointText;
+
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+
+         this.gameoverText = GameObject.Find("GameOverText");
+
+        this.PointText = GameObject.Find("PointText");
+
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+
+       
+
+        
+        }
+    void OnCollisionEnter(Collision other)
+    {
+        //小さい星に当たった場合
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            Point = Point + 1;
+        }
+        //大きい星に当たった場合
+        if (other.gameObject.tag == "LargeStarTag")
+        {
+            Point = Point + 2;
+        }
+        Debug.Log(Point);
+        this.PointText.GetComponent<Text>().text = Point.ToString();
+    }
+
+
+
+
+}
---
+++ Assets/PointText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PointText : MonoBehaviour
+{
+    //ポイントを表示する
+    private GameObject Point;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のPointオブジェクトを取得
+        this.Point= GameObject.Find("Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+}
